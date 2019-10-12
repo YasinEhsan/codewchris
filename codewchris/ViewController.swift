c789e649f7c9868d5a6289074926425d0400544f
yasinehsan <yasinehsan11@gmail.com>
2019-10-12 10:49:34 -0400

    added calls

--- codewchris/ViewController.swift
+++ codewchris/ViewController.swift
@@ -7,13 +7,25 @@
 //
 
 import UIKit
+import FirebaseDatabase
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        let ref = Database.database().reference()
+        ref.child("someid/andy").setValue(["age":"15", "height":"5'3", "sex":"male"])//dictionary key value
+        ref.child("someid/hanna").setValue(["age":12])
+        ref.childByAutoId().setValue(["age":"12"])
+        
+        ref.child("someid").observeSingleEvent(of: .value) { (DataSnapshot) in
+            let name = snapshot
+        }
+        
+        
     }
+    
+    
 
 
 }
